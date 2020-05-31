434a4045961a6fe6e730a5eec8d18b31e4aa94f7
yogeshyadav1998 <yogeshbhakli@gmail.com>
2020-05-31 17:23:10 +0530

    patientdashboard

--- pages/patientdashboard/App.js
+++ pages/patientdashboard/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LandingPage from "../Pages/LandingPage/LandingPage";
-import Dashboard from "../Pages/Dashboard/Dashboard";
-import FinalDashboard from "../Pages/FinalDashboard/FinalDashboard";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import FinalDashboard from "./Pages/FinalDashboard/FinalDashboard";
 function App() {
   return (
     <div>
